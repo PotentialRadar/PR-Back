dd61c49252d7c17c201b2d0937d3b029f422a0d4
0630hwi <148223149+0630hwi@users.noreply.github.com>
2025-07-24 17:03:40 +0900

    Update ProjectMemberRespository.java

--- src/main/java/com/potential_radar/PR/project/repository/ProjectMemberRespository.java
+++ src/main/java/com/potential_radar/PR/project/repository/ProjectMemberRespository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProjectMemberRespository extends JpaRepository<ProjectMember, Long> {
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     List<ProjectMember> findByProject_ProjectId(Long projectId);
 
     boolean existsByProject_ProjectIdAndUser_UserId(Long projectId, Long userId);

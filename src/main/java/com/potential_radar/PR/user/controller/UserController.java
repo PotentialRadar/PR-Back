b13be7fee35f1ca9a24fd165e9d78759dbd3c57d
hwi <awdqse6312@naver.com>
2025-08-18 17:54:08 +0900

    feat/좋아요(프로젝트 api), Usercontroller -> 프로젝트 좋아요(getmapping)

--- src/main/java/com/potential_radar/PR/user/controller/UserController.java
+++ src/main/java/com/potential_radar/PR/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.potential_radar.PR.user.controller;
 
+import com.potential_radar.PR.user.domain.User;
 import com.potential_radar.PR.user.dto.*;
 import com.potential_radar.PR.like.service.LikeService;
 import com.potential_radar.PR.project.dto.ProjectRecruitmentResponse;

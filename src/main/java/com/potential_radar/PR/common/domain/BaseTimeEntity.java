0d7c98b0736f11f4720f13d877ee258078ab3bf0
keemzleun <jin9ni0120@naver.com>
2025-08-21 11:22:42 +0900

    fix: sql문 통합 및 TestDataInitializer 생성

--- src/main/java/com/potential_radar/PR/common/domain/BaseTimeEntity.java
+++ src/main/java/com/potential_radar/PR/common/domain/BaseTimeEntity.java
@@ -11,8 +11,8 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class BaseTimeEntity {
     @CreationTimestamp
-    private LocalDateTime createdTime;
+    private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    private LocalDateTime updatedTime;
+    private LocalDateTime updatedAt;
 }
